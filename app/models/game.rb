3afad5910a543aab0387170202111c28775de24c
Francisco Elías <fielias81@gmail.com>
2021-07-04 19:34:48 -0300

    Fix(Game Model): dates are now properly validated

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,13 +8,17 @@ class Game < ApplicationRecord
   validates :anti_tamper, presence: true, unless: :unreleased?
   validates :image_url, presence: true, unless: :unreleased?
   validates :store_url, presence: true, unless: :unreleased?
-  validates :cracked_by, presence: true, unless: :unreleased?
-  validates :crack_date, presence: true, unless: :unreleased?
+  validates :cracked_by, presence: false, if: :cracked?
+  validates :crack_date, presence: true, if: :cracked?
 
   scope :unreleased, -> { where(status: 'unreleased') }
   scope :uncracked, -> { where(status: 'uncracked') }
 
   def unreleased?
     status == 'unreleased'
+  end
+
+  def cracked?
+    status == 'cracked'
   end
 end
